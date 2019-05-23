57f96cde69988499a965cf2b3aa324a3d810b2c2
Anderson Alves <anderson.alves_ce@outlook.com>
2019-05-23 01:23:09 -0300

    Lógica de abrir modal de esquecer a senha realizada. Falta lógica para esquecer senha.

--- public/js/index.js
+++ public/js/index.js
@@ -27,4 +27,38 @@ $(document).ready(function () {
             }
         });
     });
+
+    $("#myFormRemmemberPass").submit(function () {
+        var dados = new FormData(this);
+        $.ajax({
+            // Fazer rota de esqueceu senha
+            url: "/login",
+            type: 'POST',
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function () {
+
+            },
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: dados,
+            success: function (data, textStatus, jqXHR) {
+                window.location = '/home';
+            }, error: function (data, textStatus, errorThrown) {
+                var erros = $.parseJSON(data.responseText);
+                $(".mensagensErros").empty();
+                $.each(erros.errors, function (key, value) {
+                    $(".mensagensErros").append('<div class="alert alert-danger" role="alert" id="mensagemErro">' + value + '</div>');
+                });
+            },
+            complete: function () {
+            }
+        });
+    });
 });
+
+function closeModalLogin() {
+    $("#buy-ticket-modal").modal('hide');
+}
