042915a8746eeedadbcfc3953a660e4ebaa54e6b
cti.maracanau IFCE <cti.maracanau@ifce.edu.br>
2019-06-03 14:14:42 -0300

    Select em HomeController para index.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,16 +9,14 @@ use App\User;
 use Illuminate\Support\Facades\Response;
 use App\Kit;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -27,8 +25,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         $title = "Anna Jéssica Oficial";
         $eventoquadro = DB::select("SELECT evento.*,
         (SELECT GROUP_CONCAT(link_evento.nome_link SEPARATOR ',') FROM link_evento WHERE link_evento.id_evento_fk = evento.id_evento) as nomeLinkEvento,
@@ -36,25 +33,57 @@ class HomeController extends Controller
         from evento WHERE evento.tipo = 'Quadro' limit 4");
 
         $anuncioClassificacao1 = DB::select('SELECT * from anuncio WHERE classificacao_id_classificacao = 1 AND status = 1');
-    $anuncioClassificacao2 = DB::select('SELECT * from anuncio WHERE classificacao_id_classificacao = 2 AND status = 1');
-    $anuncioClassificacao3 = DB::select('SELECT * from anuncio WHERE classificacao_id_classificacao = 3 AND status = 1');
-    $slideshows = DB::select('SELECT * from slideshow WHERE status = 1 limit 3');
+        $anuncioClassificacao2 = DB::select('SELECT * from anuncio WHERE classificacao_id_classificacao = 2 AND status = 1');
+        $anuncioClassificacao3 = DB::select('SELECT * from anuncio WHERE classificacao_id_classificacao = 3 AND status = 1');
+        $slideshows = DB::select('SELECT * from slideshow WHERE status = 1 limit 3');
 
-    return view('index', compact('title','eventoquadro', 'anuncioClassificacao1', 'anuncioClassificacao2', 'anuncioClassificacao3', 'slideshows'));
+        $datas = DB::select("SELECT 
+        LEFT(lower(DATE_FORMAT(data_inicio, '%d')), 3) AS dia,
+        LEFT(lower(DATE_FORMAT(data_inicio, '%M')), 3) AS mes,
+        RIGHT(UPPER(DATE_FORMAT(data_inicio, '%Y')), 2) AS ano,
+        DATE_FORMAT(data_inicio, '%Y') as numeroAno,
+        DATE_FORMAT(data_inicio, '%m') as numeroMes
+        FROM agenda 
+        GROUP BY mes, ano, numeroMes, numeroAno, agenda.data_inicio
+        order by numeroMes asc, dia asc");
+
+        $agendas = DB::select("SELECT 
+        LEFT(lower(DATE_FORMAT(data_inicio, '%d')), 3) AS dia,
+        LEFT(lower(DATE_FORMAT(data_inicio, '%M')), 3) AS mes,
+        RIGHT(UPPER(DATE_FORMAT(data_inicio, '%Y')), 2) AS ano,
+        DATE_FORMAT(data_inicio, '%Y') as numeroAno,
+        DATE_FORMAT(data_inicio, '%m') as numeroMes,
+        (SELECT GROUP_CONCAT(agen.id_agenda SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as idEvento,
+        (SELECT GROUP_CONCAT(agen.nome_evento SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as nomeEvento,
+        (SELECT GROUP_CONCAT(agen.descricao SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as descricaoEvento,
+        (SELECT GROUP_CONCAT(agen.imagem SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as imagemEvento,
+        (SELECT GROUP_CONCAT(agen.hora_inicio SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as horaInicio,
+        (SELECT GROUP_CONCAT(agen.hora_fim SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as horaFim,
+                (SELECT GROUP_CONCAT(agen.cidade SEPARATOR ', ') 
+                FROM agenda agen WHERE agen.data_inicio = agenda.data_inicio) as cidade
+        FROM agenda 
+        GROUP BY mes, ano, numeroMes, numeroAno, agenda.data_inicio
+        order by numeroMes asc, dia asc");
+
+        return view('index', compact('title', 'eventoquadro', 'anuncioClassificacao1', 'anuncioClassificacao2', 'anuncioClassificacao3', 'slideshows', 'agendas', 'datas'));
     }
 
-    public function token(Request $request)
-    {
+    public function token(Request $request) {
         $token = base64_decode($request->token);
         $ativar = DB::table('usuario')->where('email', $token)->update(array(
             'ativo_usuario' => 1,
         ));
         return route('home')
-            ->with('cadastrado', 'cadastrado');
+                        ->with('cadastrado', 'cadastrado');
     }
 
-    public function adminConf()
-    {
+    public function adminConf() {
         if (auth()->user()->id_tipo_usuario == 1) {
             $title = "Administrador";
             return view('adminConf', compact('title'));
@@ -63,8 +92,7 @@ class HomeController extends Controller
         return back();
     }
 
-    public function pageRelatorioUser()
-    {
+    public function pageRelatorioUser() {
         if (auth()->user()->id_tipo_usuario == 2) {
             $title = "Relatório";
             return view('user.pageRelatorioUser', compact('title'));
@@ -73,8 +101,7 @@ class HomeController extends Controller
         return back();
     }
 
-    public function compraKit()
-    {
+    public function compraKit() {
         $title = "Compra Kit";
         $selectKits = DB::select("
         SELECT evento.*,
@@ -85,24 +112,21 @@ class HomeController extends Controller
         (SELECT GROUP_CONCAT(kit_evento.id_tamanho SEPARATOR ',') FROM kit_evento WHERE kit_evento.id_evento_fk = evento.id_evento) as nomeLinkEvento,
         (SELECT GROUP_CONCAT(kit_evento.descricao_kit SEPARATOR ',') FROM kit_evento WHERE kit_evento.id_evento_fk = evento.id_evento) as linkEvento
         from evento WHERE evento.tipo = 'Destaque' limit 1
-        "); 
+        ");
         return view('user.compraKit', compact('title', 'selectKits'));
     }
 
-    public function getKit(Request $request)
-    {
-        $kit=Kit::where('id_kit','=',$request->idKit)->get();
+    public function getKit(Request $request) {
+        $kit = Kit::where('id_kit', '=', $request->idKit)->get();
         return response()->json($kit);
     }
 
-    public function perfil()
-    {
+    public function perfil() {
         $title = "Perfil";
         return view('perfil', compact('title'));
     }
 
-    public function editUser(Request $request)
-    {
+    public function editUser(Request $request) {
         $this->validateUserEdit($request);
 
         if ($request->password != null) {
@@ -119,14 +143,14 @@ class HomeController extends Controller
         return Response::json($request);
     }
 
-    protected function validateUserEdit(Request $request)
-    {
+    protected function validateUserEdit(Request $request) {
         return $this->validate($request, [
-            'nome' => 'required',
-            'telefone' => 'required',
-            'email' => 'required',
-            'cidade' => 'required',
-            'password' => 'confirmed',
+                    'nome' => 'required',
+                    'telefone' => 'required',
+                    'email' => 'required',
+                    'cidade' => 'required',
+                    'password' => 'confirmed',
         ]);
     }
+
 }
